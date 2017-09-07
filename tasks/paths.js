3f99fc2893abf55c6171fb9c98076b95d9e25441
Malgorne <fritz.benj@free.fr>
2017-09-07 19:33:18 +0200

    Cleans test task.

--- tasks/paths.js
+++ tasks/paths.js
@@ -1,10 +1,10 @@
 export default {
   doc: ['src/app/index.js', 'README.md'],
   appTemplates: ['src/app/templates/**/.*', 'src/app/templates/**/*'],
-  routeTemplates: ['src/route/templates/**/.*', 'src/route/templates/**/*'],
-  exceptTemplates: ['!src/app/templates/**/*', '!src/route/templates/**/*'],
-  jsExceptTemplates: ['src/**/*.js', '!src/app/templates/**/*', '!src/route/templates/**/*'],
+  routeTemplates: ['src/route/templates/**/.*'],
+  exceptTemplates: ['!src/app/templates/**/*'],
+  jsExceptTemplates: ['src/**/*.js', '!src/app/templates/**/*'],
   cleanProject: ['generators', 'doc', 'coverage'],
-  coverage: ['./generators/app/*.js', './generators/route/*.js', '!./generators/app/templates/**/*', '!./generators/route/templates/**/*'],
+  coverage: ['./generators/app/*.js', '!./generators/app/templates/**/*'],
   tests: './tests/*.test.js'
 };
